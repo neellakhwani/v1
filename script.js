ca807f2b62c5460c5cab3aee2a098763d385a8f5
Neel Lakhwani <neel.lakhwani99@gmail.com>
2020-04-27 17:22:04 +0530

    new animation
    content fade in

--- script.js
+++ script.js
@@ -14,22 +14,42 @@
 
      if(currentSlide == 0) {
 	  	document.body.style.background = '#004A5A';
+	  	document.getElementById("slide1").classList.add("invisible");
+	  	document.getElementById("slide2").classList.add("invisible");
+	  	document.getElementById("slide3").classList.add("invisible");
+	  	document.getElementById("slide4").classList.add("invisible");
      }
 
      if(currentSlide == 1) {
 	  	document.body.style.background = '#282828';
+	  	document.getElementById("slide1").classList.remove("invisible");
+	  	document.getElementById("slide2").classList.add("invisible");
+	  	document.getElementById("slide3").classList.add("invisible");
+	  	document.getElementById("slide4").classList.add("invisible");
      }
 
      if(currentSlide == 2) {
 	  	document.body.style.background = '#F479D1';
+	  	document.getElementById("slide1").classList.add("invisible");
+	  	document.getElementById("slide2").classList.remove("invisible");
+	  	document.getElementById("slide3").classList.add("invisible");
+	  	document.getElementById("slide4").classList.add("invisible");
      }
 
      if(currentSlide == 3) {
 	  	document.body.style.background = '#ffffff';
+	  	document.getElementById("slide1").classList.add("invisible");
+	  	document.getElementById("slide2").classList.add("invisible");
+	  	document.getElementById("slide3").classList.remove("invisible");
+	  	document.getElementById("slide4").classList.add("invisible");
      }
 
      if(currentSlide == 4) {
 	  	document.body.style.background = '#5EBE87';
+	  	document.getElementById("slide1").classList.add("invisible");
+	  	document.getElementById("slide2").classList.add("invisible");
+	  	document.getElementById("slide3").classList.add("invisible");
+	  	document.getElementById("slide4").classList.remove("invisible");
      }
  }
 
