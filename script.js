6c191ed27459b8783b87703fb8e35089dc194852
neellakhwani <neel.lakhwani99@gmail.com>
2023-05-12 15:12:44 +0530

    Added Cipher Docs and minor changes
    Added Cipher Docs and padding for project-subtitle

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 	document.getElementById("slide3").style.background = "transparent";
 	document.getElementById("slide4").style.background = "transparent";
   document.getElementById("slide5").style.background = "transparent";
+  document.getElementById("slide6").style.background = "transparent";
   setBackground();
 }
 
@@ -19,45 +20,67 @@
 	  	document.getElementById("slide2").classList.add("invisible");
 	  	document.getElementById("slide3").classList.add("invisible");
 	  	document.getElementById("slide4").classList.add("invisible");
+		document.getElementById("slide5").classList.add("invisible");
+		document.getElementById("slide6").classList.add("invisible");
      }
 
      if(currentSlide == 1) {
-	  	document.body.style.background = '#8C7BD1';
+	  	document.body.style.background = '#933966';
 	  	document.getElementById("slide1").classList.remove("invisible");
 	  	document.getElementById("slide2").classList.add("invisible");
 	  	document.getElementById("slide3").classList.add("invisible");
 	  	document.getElementById("slide4").classList.add("invisible");
+		document.getElementById("slide5").classList.add("invisible");
+		document.getElementById("slide6").classList.add("invisible");
      }
 
-     if(currentSlide == 2) {
-	  	document.body.style.background = '#282828';
-	  	document.getElementById("slide1").classList.add("invisible");
-	  	document.getElementById("slide2").classList.remove("invisible");
-	  	document.getElementById("slide3").classList.add("invisible");
-	  	document.getElementById("slide4").classList.add("invisible");
-     }
+	 if(currentSlide == 2) {
+		document.body.style.background = '#8C7BD1';
+		document.getElementById("slide1").classList.add("invisible");
+		document.getElementById("slide2").classList.remove("invisible");
+		document.getElementById("slide3").classList.add("invisible");
+		document.getElementById("slide4").classList.add("invisible");
+		document.getElementById("slide5").classList.add("invisible");
+		document.getElementById("slide6").classList.add("invisible");
+   }
 
      if(currentSlide == 3) {
-	  	document.body.style.background = '#F479D1';
+	  	document.body.style.background = '#282828';
 	  	document.getElementById("slide1").classList.add("invisible");
-	  	document.getElementById("slide2").classList.add("invisible");
+		document.getElementById("slide2").classList.add("invisible");
 	  	document.getElementById("slide3").classList.remove("invisible");
 	  	document.getElementById("slide4").classList.add("invisible");
+	  	document.getElementById("slide5").classList.add("invisible");
+		document.getElementById("slide6").classList.add("invisible");
      }
 
      if(currentSlide == 4) {
+	  	document.body.style.background = '#F479D1';
+	  	document.getElementById("slide1").classList.add("invisible");
+		document.getElementById("slide2").classList.add("invisible");
+	  	document.getElementById("slide3").classList.add("invisible");
+	  	document.getElementById("slide4").classList.remove("invisible");
+	  	document.getElementById("slide5").classList.add("invisible");
+		document.getElementById("slide6").classList.add("invisible");
+     }
+
+     if(currentSlide == 5) {
 	  	document.body.style.background = '#ffffff';
 	  	document.getElementById("slide1").classList.add("invisible");
 	  	document.getElementById("slide2").classList.add("invisible");
 	  	document.getElementById("slide3").classList.add("invisible");
-	  	document.getElementById("slide4").classList.remove("invisible");
+		document.getElementById("slide4").classList.add("invisible");
+	  	document.getElementById("slide5").classList.remove("invisible");
+		document.getElementById("slide6").classList.add("invisible");
      }
 
-     if(currentSlide == 5) {
+     if(currentSlide == 6) {
 	  	document.body.style.background = '#5EBE87';
 	  	document.getElementById("slide1").classList.add("invisible");
 	  	document.getElementById("slide2").classList.add("invisible");
 	  	document.getElementById("slide3").classList.add("invisible");
-	  	document.getElementById("slide4").classList.remove("invisible");
+		document.getElementById("slide4").classList.add("invisible");
+		document.getElementById("slide5").classList.add("invisible");
+	  	document.getElementById("slide6").classList.remove("invisible");
      }
  }
